0a37cf7430aa74dd8748b25a01e54e37fc4b4e8b
Kiran Anand <liteninkiran@hotmail.com>
2023-07-30 01:07:29 +0100

    Add update page (member)

--- controllers/teamListingController.js
+++ controllers/teamListingController.js
@@ -117,6 +117,50 @@ exports.viewMember = async (req, res) => {
 
 /**
  * GET /
+ * Update Member Form
+ */
+exports.editMember = async (req, res) => {
+
+    const sql = 'SELECT m.id, m.member_name FROM members AS m WHERE m.id = ?';
+    db.all(sql, req.params.id, (err, data) => {
+        if (err) {
+            return res.json({
+                status: 300,
+                success: false,
+                error: err,
+            });
+        }
+        if (data.length === 1) {
+            res.render('member/edit', { data: data[0] });
+        } else {
+            res.render('404');
+        }
+    });
+};
+
+/**
+ * PUT /
+ * Update Member
+ */
+exports.updateMember = async (req, res) => {
+
+    const sql = 'UPDATE members SET member_name = ? WHERE id = ?';
+
+    db.run(sql, [req.body.memberName, req.params.id], (err) => {
+        if (err) {
+            return res.json({
+                status: 300,
+                success: false,
+                error: err,
+            });
+        }
+    });
+
+    res.redirect('/');
+};
+
+/**
+ * GET /
  * Update Team Form
  */
 exports.editTeam = async (req, res) => {
--- routes/teamListing.js
+++ routes/teamListing.js
@@ -7,6 +7,8 @@ router.delete('/members/delete/:id', teamListingController.deleteMember);
 router.delete('/teams/delete/:id', teamListingController.deleteTeam);
 router.get('/members/view/:id', teamListingController.viewMember);
 router.get('/teams/view/:id', teamListingController.viewTeam);
+router.get('/members/edit/:id', teamListingController.editMember);
+router.put('/members/edit/:id', teamListingController.updateMember);
 router.get('/teams/edit/:id', teamListingController.editTeam);
 router.put('/teams/edit/:id', teamListingController.updateTeam);
 
